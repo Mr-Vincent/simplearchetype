5386cf63719caf58015e8b0e259379a5468934bb
wei.dong <wei.dong@ectrip.com>
2018-03-02 14:04:50 +0800

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author dongwei
+ * @date 2018/03/02
+ * Time: 12:55
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
